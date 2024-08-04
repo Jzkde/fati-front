b9b7fe3f75d04a19ffee0b3290f1fa8bf04788b2
Diego Adrian Gallardo <dagjjj@gmail.com>
2024-08-04 19:42:25 -0300

    Listo para despliegue

--- src/app/service/api/api.ts
+++ src/app/service/api/api.ts
@@ -1,10 +1,10 @@
 export const API = {
 
   //para el despliegue en VPS
-  //URL: "http://192.168.0.107:8081/"
+  URL: "http://192.168.0.107:8081/"
 
   //para el proceso de construccion
-  URL: "http://localhost:8081/"
+  //URL: "http://localhost:8081/"
 
   //para publicar en portfolio
   //URL:"https://fati-decoraciones.onrender.com/"
